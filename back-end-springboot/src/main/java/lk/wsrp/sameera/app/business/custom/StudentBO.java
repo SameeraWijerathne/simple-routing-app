caccf27ca80e831af89595161bc0045c132e5ea2
Sameera <sameerawncd@gmail.com>
2023-07-01 16:47:12 +0530

    Finalized the Business layer

--- back-end-springboot/src/main/java/lk/wsrp/sameera/app/business/custom/StudentBO.java
+++ back-end-springboot/src/main/java/lk/wsrp/sameera/app/business/custom/StudentBO.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface StudentBO {
     List<StudentDTO> getAllStudents() throws Exception;
 
-    StudentDTO saveStudent() throws Exception;
+    StudentDTO saveStudent(StudentDTO studentDTO) throws Exception;
 
-    void deleteStudent() throws Exception;
+    void deleteStudent(int studentId) throws Exception;
 }
--- back-end-springboot/src/main/java/lk/wsrp/sameera/app/business/custom/impl/StudentBOImpl.java
+++ back-end-springboot/src/main/java/lk/wsrp/sameera/app/business/custom/impl/StudentBOImpl.java
@@ -1,24 +1,44 @@
 package lk.wsrp.sameera.app.business.custom.impl;
 
 import lk.wsrp.sameera.app.business.custom.StudentBO;
+import lk.wsrp.sameera.app.business.exception.DuplicateRecordException;
+import lk.wsrp.sameera.app.business.exception.RecordNotFoundException;
+import lk.wsrp.sameera.app.business.util.Transformer;
+import lk.wsrp.sameera.app.dao.custom.StudentDAO;
 import lk.wsrp.sameera.app.dto.StudentDTO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@Transactional
 public class StudentBOImpl implements StudentBO {
+    private final StudentDAO studentDAO;
+    private final Transformer transformer;
+
+    public StudentBOImpl(StudentDAO studentDAO, Transformer transformer) {
+        this.studentDAO = studentDAO;
+        this.transformer = transformer;
+    }
+
+    @Transactional(readOnly = true)
     @Override
     public List<StudentDTO> getAllStudents() throws Exception {
-        return null;
+        return studentDAO.findAll().stream().map(transformer::fromStudentEntity).collect(Collectors.toList());
     }
 
     @Override
-    public StudentDTO saveStudent() throws Exception {
-        return null;
+    public StudentDTO saveStudent(StudentDTO studentDTO) throws Exception {
+        return transformer.fromStudentEntity(studentDAO.save(transformer.toStudentEntity(studentDTO)));
     }
 
     @Override
-    public void deleteStudent() throws Exception {
-
+    public void deleteStudent(int studentId) throws Exception {
+        if (!studentDAO.existsById(studentId)) {
+            throw new RecordNotFoundException("No record was found associate with the id: " + studentId);
+        }
+        studentDAO.deleteById(studentId);
     }
 }
