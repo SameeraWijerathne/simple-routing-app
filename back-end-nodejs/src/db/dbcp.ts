4f4d16b4e0a7ced0be1bd025f5f7644e79f0390a
Sameera <sameerawncd@gmail.com>
2023-07-01 19:27:40 +0530

    Database connection pool was created

---
+++ back-end-nodejs/src/db/dbcp.ts
@@ -0,0 +1,16 @@
+import mysql, {Pool} from 'promise-mysql';
+import env from 'dotenv';
+
+env.config();
+export let pool: Pool;
+
+(async function () {
+    pool = await mysql.createPool({
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT!,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        connectionLimit: +process.env.DB_CP_SIZE!
+    });
+})();
