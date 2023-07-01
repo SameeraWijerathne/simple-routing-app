24999f77320a158accd3b2c50161fd6cbec08647
Sameera <sameerawncd@gmail.com>
2023-07-01 19:38:50 +0530

    Server setup implemented

---
+++ back-end-nodejs/src/main.ts
@@ -0,0 +1,10 @@
+import express, {json} from 'express';
+import {router as CourseRouter} from "./api/course-http-controller";
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use("/api/v1/courses", CourseRouter);
+app.listen(8080, () => console.log("Server has been started at 8080"));
