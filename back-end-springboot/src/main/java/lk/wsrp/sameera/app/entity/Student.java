51ad3f4d525d8f54f0d0178e5c4613b7a0af163d
Sameera <sameerawncd@gmail.com>
2023-07-01 15:20:17 +0530

    Entity class created

---
+++ back-end-springboot/src/main/java/lk/wsrp/sameera/app/entity/Student.java
@@ -0,0 +1,21 @@
+package lk.wsrp.sameera.app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    private int id;
+    private String name;
+    private String address;
+
+    public Student(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
---
+++ back-end-springboot/src/main/java/lk/wsrp/sameera/app/entity/SuperEntity.java
@@ -0,0 +1,6 @@
+package lk.wsrp.sameera.app.entity;
+
+import java.io.Serializable;
+
+public interface SuperEntity extends Serializable {
+}
