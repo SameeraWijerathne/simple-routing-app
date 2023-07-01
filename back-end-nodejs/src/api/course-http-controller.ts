48a7864d59fa7e007f0ab0a6a0c4b16180aaffa9
Sameera <sameerawncd@gmail.com>
2023-07-01 20:17:07 +0530

    saveCourse web service exposed

--- back-end-nodejs/src/api/course-http-controller.ts
+++ back-end-nodejs/src/api/course-http-controller.ts
@@ -14,3 +14,13 @@ router.get('/', async (req, res)=>{
     res.json(courses);
 });
 
+router.post('/', async (req, res)=>{
+    const course = (req.body as Course);
+    if (!course.id?.trim() || !course.description?.trim() || +course.duration < 0 || !course.duration) {
+        res.sendStatus(400);
+        return;
+    }
+    const result = await pool.query("INSERT INTO course (id, description, duration) VALUES (?,?,?)",
+        [course.id, course.description, course.duration]);
+    res.status(201).json(course);
+});
