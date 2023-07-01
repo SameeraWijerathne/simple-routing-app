d5a876b69737b2087e8039caf196462cdb3bde84
Sameera <sameerawncd@gmail.com>
2023-07-01 17:06:02 +0530

    Finalized the app with a global exception handler

---
+++ back-end-springboot/src/main/java/lk/wsrp/sameera/app/advice/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package lk.wsrp.sameera.app.advice;
+
+import lk.wsrp.sameera.app.business.exception.BOException;
+import lk.wsrp.sameera.app.business.exception.DuplicateRecordException;
+import lk.wsrp.sameera.app.business.exception.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BOException.class)
+    public void handleRecordNotFoundException(BOException e) {
+        ResponseStatusException exp;
+        if (e instanceof RecordNotFoundException) {
+            exp = new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } else if (e instanceof DuplicateRecordException) {
+            exp = new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        } else {
+            exp = new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
+        }
+        e.initCause(exp);
+        throw e;
+    }
+}
