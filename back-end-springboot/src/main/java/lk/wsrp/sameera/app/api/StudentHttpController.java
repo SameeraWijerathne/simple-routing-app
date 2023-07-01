71eabb38e18439f62ace81e1e9bfc165127970a8
Sameera <sameerawncd@gmail.com>
2023-07-01 15:11:32 +0530

    main web services exposed

--- back-end-springboot/src/main/java/lk/wsrp/sameera/app/api/StudentHttpController.java
+++ back-end-springboot/src/main/java/lk/wsrp/sameera/app/api/StudentHttpController.java
@@ -1,23 +1,31 @@
 package lk.wsrp.sameera.app.api;
 
+import lk.wsrp.sameera.app.dto.StudentDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/v1/students")
 public class StudentHttpController {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @GetMapping
-    public String getAllStudents() {
-        return "get()";
+    public List<StudentDTO> getAllStudents() {
+        return null;
     }
 
-    @PostMapping
-    public String saveStudent() {
-        return "post()";
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "application/json")
+    public StudentDTO saveStudent(@RequestBody @Valid StudentDTO student) {
+        return null;
     }
 
-    @DeleteMapping
-    public String deleteStudent() {
-        return "delete()";
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Integer studentId) {
     }
 }
