de3d0437a1dfcecf74f6fd37fdb876300e60d375
Sameera <sameerawncd@gmail.com>
2023-07-01 16:57:13 +0530

    Three web services exposed at the API layer

--- back-end-springboot/src/main/java/lk/wsrp/sameera/app/api/StudentHttpController.java
+++ back-end-springboot/src/main/java/lk/wsrp/sameera/app/api/StudentHttpController.java
@@ -1,5 +1,6 @@
 package lk.wsrp.sameera.app.api;
 
+import lk.wsrp.sameera.app.business.custom.StudentBO;
 import lk.wsrp.sameera.app.dto.StudentDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,21 +12,28 @@ import java.util.List;
 @CrossOrigin
 @RequestMapping("/api/v1/students")
 public class StudentHttpController {
+    private final StudentBO studentBO;
+
+    public StudentHttpController(StudentBO studentBO) {
+        this.studentBO = studentBO;
+    }
+
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @GetMapping
-    public List<StudentDTO> getAllStudents() {
-        return null;
+    public List<StudentDTO> getAllStudents() throws Exception {
+        return studentBO.getAllStudents();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "application/json")
-    public StudentDTO saveStudent(@RequestBody @Valid StudentDTO student) {
-        return null;
+    public StudentDTO saveStudent(@RequestBody @Valid StudentDTO student) throws Exception {
+        return studentBO.saveStudent(student);
     }
 
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{studentId}")
-    public void deleteStudent(@PathVariable("studentId") Integer studentId) {
+    public void deleteStudent(@PathVariable("studentId") Integer studentId) throws Exception {
+        studentBO.deleteStudent(studentId);
     }
 }
