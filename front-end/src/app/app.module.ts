94314c572e8b9a11437e26aba2c1347e90bb1f39
Sameera <sameerawncd@gmail.com>
2023-07-13 12:43:30 +0530

    Basic structure of the frontend UI is created with Angular routing

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,13 +2,47 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import { ManageStudentsComponent } from './manage-students/manage-students.component';
+import { ManageCoursesComponent } from './manage-courses/manage-courses.component';
+import {FormsModule} from "@angular/forms";
 
+const routes: Routes = [
+  {
+    path: "students",
+    component: ManageStudentsComponent
+  },
+  {
+    path: "courses",
+    component: ManageCoursesComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ManageStudentsComponent,
+    ManageCoursesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
