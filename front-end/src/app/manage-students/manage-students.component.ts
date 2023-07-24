97d32236337adddef404aeff44c4a573d43f9d23
Sameera <sameerawncd@gmail.com>
2023-07-25 00:15:41 +0530

    Finalize manage-student UI

--- front-end/src/app/manage-students/manage-students.component.ts
+++ front-end/src/app/manage-students/manage-students.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Student} from "../dto/student";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-manage-students',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./manage-students.component.scss']
 })
 export class ManageStudentsComponent {
+  studentList: Array<Student> = [];
 
+  constructor(private http: HttpClient) {
+    http.get<Array<Student>>('http://localhost:8080/app/api/v1/students')
+      .subscribe(studentList => this.studentList = studentList);
+  }
 }
